c7f77729c0fe58a7972860e0230c2496d6f7c14e
Felipe <felipeonlinebr@gmail.com>
2023-06-21 20:19:14 -0300

    Patch consertado

--- NimbusSync/NimbusSync.Server.Domain/TecnicalDraw.cs
+++ NimbusSync/NimbusSync.Server.Domain/TecnicalDraw.cs
@@ -4,14 +4,14 @@ namespace NimbusSync.Server.Domain
 {
     public class TecnicalDraw
     {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public byte[] File { get; set; }
-        public DateTime CreationDate { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public Account Author { get; set; }
-        public string AuthorId { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public byte[]? File { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public Account? Author { get; set; }
+        public string? AuthorId { get; set; }
 
         //Criar usando os materiais
         public static string GenerateCode(TecnicalDraw tecnicalDraw)
